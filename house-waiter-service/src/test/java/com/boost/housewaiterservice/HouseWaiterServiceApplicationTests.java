e56a19938af0cf0a276618f8236792ef35e694d0
tburiak <tburiak@playtika.com>
2021-06-01 00:04:06 +0300

    [PT-1]: initialize project

---
+++ house-waiter-service/src/test/java/com/boost/housewaiterservice/HouseWaiterServiceApplicationTests.java
@@ -0,0 +1,13 @@
+package com.boost.housewaiterservice;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class HouseWaiterServiceApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
